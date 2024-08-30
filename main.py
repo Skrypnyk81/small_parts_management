23f3d3a815eee768b26f7b9d5977827f223c282b
Skrypnyk Yuriy <skrypnyk81@gmail.com>
2024-08-30 16:26:50 +0200

    Create home.py and templates

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, UploadFile, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+
+@app.get("/home/", response_class=HTMLResponse)
+async def get_home(request: Request,):
+    return templates.TemplateResponse(
+        "home.html",
+        {"request": request}
+    )
+
+
+@app.get("/upload/", response_class=HTMLResponse)
+async def get_upload(request: Request,):
+    return templates.TemplateResponse(
+        "upload.html",
+        {"request": request}
+    )
