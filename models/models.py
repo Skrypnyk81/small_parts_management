710284d62a700ab79bf3da59ec3035e50422ff15
Yuriy Skrypnyk <skrypnyk81@gmail.com>
2024-09-01 21:33:29 +0200

    Change model name from plular to single

--- models/models.py
+++ models/models.py
@@ -2,8 +2,8 @@ from sqlalchemy import MetaData, Table, Column, Integer, String, DateTime
 
 metadata = MetaData()
 
-items = Table(
-    "items",
+item = Table(
+    "item",
     metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
     Column("name", String(50), nullable=False),
